e650c2782ee237188a510cbb4e9e9b183f913e2a
wonjinSong <wjsong0204@naver.com>
2022-07-28 09:43:01 +0900

    Spring commit again

---
+++ SpringCRUD/src/test/java/com/myp/SpringCRUD/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myp.SpringCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
